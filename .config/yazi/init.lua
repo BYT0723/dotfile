322cf601213a24f0e755b2d6d1a09959a847c849
BYT0723 <twang9739@gmail.com>
2024-12-25 10:03:05 +0800

    perf: yazi lua script

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -3,4 +3,4 @@ require("smart-enter"):setup({
 })
 require("git"):setup()
 
-require("starship"):setup({ config_file = "/home/walter/.config/yazi/starship.toml" })
+require("starship"):setup({ config_file = "~/.config/yazi/starship.toml" })
